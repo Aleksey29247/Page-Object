e912e92449acba1fa4109ab012205a14771b8cfc
Aleksey <Alik29247@gmail.com>
2022-04-19 20:35:20 +0700

    initial commit

--- src/test/java/ru.netology/data/DataHelper.java
+++ src/test/java/ru.netology/data/DataHelper.java
@@ -8,34 +8,26 @@ public class DataHelper {
 
     @Value
     public static class AuthInfo{
-        private String login;
-        private String password;
+        String login;
+        String password;
 
     }
-    public static AuthInfo getAuthInfo(){
-        return new AuthInfo ("vasya", "qwerty123");
+    public static AuthInfo getAuthInfo(){ return new AuthInfo ("vasya", "qwerty123");
     }
+
+    public static AuthInfo getOtherAuthInfo(AuthInfo original){ return new AuthInfo("petya", "123qwerty");
+
+    }
+
 
     @Value
     public static class VerificationCode {
-        private String code;
+        String code;
     }
 
     public static VerificationCode getVerificationCodeFor (AuthInfo authInfo){
-         return new VerificationCode("12345");
+        return new VerificationCode("12345");
         }
 
-    @Value
-    public static class CardInfo {
-        private String summa;
-        private String number;
-    }
 
-    public static CardInfo getInfoForTransfer(AuthInfo authInfo) {
-        return new CardInfo("10000", "5559 0000 0000 0001");
-    }
-
-    public static CardInfo getOtherInfoForTransfer(AuthInfo authInfo) {
-        return new CardInfo("10000", "5559 0000 0000 0001");
-    }
 }
--- src/test/java/ru.netology/test/MoneyTransferTest.java
+++ src/test/java/ru.netology/test/MoneyTransferTest.java
@@ -1,9 +1,9 @@
 package ru.netology.test;
 
+
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataHelper;
 import ru.netology.page.LoginPageV1;
-
 
 import static com.codeborne.selenide.Selenide.open;
 
@@ -14,5 +14,8 @@ public class MoneyTransferTest {
         open("http://localhost:9999");
         var loginPage = new LoginPageV1();
         var authInfo = DataHelper.getAuthInfo();
-    }
+        var verificationPage = loginPage.validLogin(authInfo);
+
+        }
+
 }
