6d5f38e929720cbe20119936204a8b13f3a2e6d7
Aleksey <Alik29247@gmail.com>
2022-04-20 20:11:51 +0700

    initial commit

--- src/test/java/ru.netology/test/MoneyTransferTest.java
+++ src/test/java/ru.netology/test/MoneyTransferTest.java
@@ -1,6 +1,8 @@
 package ru.netology.test;
 
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataHelper;
 import ru.netology.page.LoginPageV1;
@@ -9,12 +11,21 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class MoneyTransferTest {
 
+
+
     @Test
     void shouldTransferMoneyBetweenOwnCardsV1() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "800x800";
         open("http://localhost:9999");
         var loginPage = new LoginPageV1();
         var authInfo = DataHelper.getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
+        var verifyInfo = DataHelper.getVerificationCodeFor(authInfo);
+        var dashboardPage = verificationPage.validVerify(verifyInfo);
+
+
+
 
         }
 
